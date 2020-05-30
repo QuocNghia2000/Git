946444df1a1bfcc67ee467939a9a06be43812ed5
Le Nghia <nghja1832000@gmail.com>
2020-05-30 17:06:02 +0700

    Add project files.

---
+++ Form6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ĐACNPM
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    public void Show()
+        {
+            DataTable dt = new DataTable();
+
+        }
+    }
+}
---
+++ Form7.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ĐACNPM
+{
+    public partial class Form7 : Form
+    {
+        public Form7()
+        {
+            InitializeComponent();
+            dateTimePicker1.Value = Convert.ToDateTime("2020/09/09");
+        }
+    }
+}
